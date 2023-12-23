446cb633a080fecd18ae9026f74d858619a43373
Ronald Record <github@ronrecord.com>
2023-12-22 16:31:33 -0800

    Sync with Cloudflare

--- dnsconfig.js
+++ dnsconfig.js
@@ -1,13 +1,17 @@
-var DSP_CLOUDFLARE = NewDnsProvider("cloudflare");
-var REG_NONE = NewRegistrar("none");
+var DSP_CLOUDFLARE = NewDnsProvider('cloudflare');
+var REG_NONE = NewRegistrar('none');
 
-D("neoman.dev", REG_NONE, DnsProvider(DSP_CLOUDFLARE),
+D('neoman.dev', REG_NONE, DnsProvider(DSP_CLOUDFLARE),
     A('@', '185.199.111.153', CF_PROXY_ON),
     A('@', '185.199.110.153', CF_PROXY_ON),
     A('@', '185.199.109.153', CF_PROXY_ON),
     A('@', '185.199.108.153', CF_PROXY_ON),
+	  A('arch', '67.180.229.153'),
+	  A('fedora', '67.180.229.153'),
+	  A('kasm', '10.0.1.120'),
     A('ronnie', '67.180.229.153'),
     CNAME('_domainconnect', 'connect.domains.google.com.', CF_PROXY_ON),
+	  CNAME('kasmweb', '23381d9b-272d-4418-966e-69a88080f321.cfargotunnel.com.', CF_PROXY_ON),
     CNAME('readme', 'doctorfree.github.io.', CF_PROXY_ON),
     CNAME('www', 'doctorfree.github.io.', CF_PROXY_ON),
     MX('@', 40, 'alt4.gmr-smtp-in.l.google.com.', TTL(3600)),
@@ -17,17 +21,18 @@ D("neoman.dev", REG_NONE, DnsProvider(DSP_CLOUDFLARE),
     MX('@', 5, 'gmr-smtp-in.l.google.com.', TTL(3600)),
     TXT('_github-pages-challenge-doctorfree', 'c258dc119431ba55f44a68878d88b1'),
     TXT('@', 'google-site-verification=1gv0rI-PWeL5b15TgaOJHJxklE-BY0iDgqz2dbbK0z8'),
+	  TXT('_acme-challenge', 'fYs1YgM1Ch2qnsPmUbnuuLDqmgak5nGk1I424JRPBso'),
     // Not doing this yet
     // CAA_BUILDER({
-    //   label: "@",
-    //   iodef: "ronaldrecord@gmail.com",
+    //   label: '@',
+    //   iodef: 'ronaldrecord@gmail.com',
     //   iodef_critical: true,
     //   issue: [
-    //     "letsencrypt.org",
-    //     "comodoca.com",
+    //     'letsencrypt.org',
+    //     'comodoca.com',
     //   ],
     //   issuewild: [
-    //     "letsencrypt.org",
+    //     'letsencrypt.org',
     //   ],
     // }),
     DMARC_BUILDER({
@@ -44,7 +49,7 @@ D("neoman.dev", REG_NONE, DnsProvider(DSP_CLOUDFLARE),
         ],
     }),
     SPF_BUILDER({
-        label: "@",
+        label: '@',
         overflow: "_spf%d",
         raw: "_rawspf",
         parts: [
